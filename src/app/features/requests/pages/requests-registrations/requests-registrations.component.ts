eb3296716662fcbdb5c9bfcbfa96a4610b1cfbce
Juan Amaya <97049133+jamayak@users.noreply.github.com>
2025-08-23 12:55:13 -0500

    feat: requests

--- src/app/features/requests/pages/requests-registrations/requests-registrations.component.ts
+++ src/app/features/requests/pages/requests-registrations/requests-registrations.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, ViewChild} from '@angular/core';
+import {IconFieldModule} from 'primeng/iconfield';
+import {InputIconModule} from 'primeng/inputicon';
+import {InputTextModule} from 'primeng/inputtext';
+import {TableModule} from 'primeng/table';
+import {PopoverModule} from 'primeng/popover';
+import {ButtonModule} from 'primeng/button';
+import {AlertsService} from '../../../../core/services/alerts.service';
+import {ConfirmationService} from 'primeng/api';
+import {DialogService, DynamicDialogRef} from 'primeng/dynamicdialog';
+import {Router} from '@angular/router';
+import {CurrencyPipe} from '@angular/common';
+import {TableSkeletonComponent} from '../../../../shared/components/skeleton/table-skeleton/table-skeleton.component';
+import {DeclarationsStatus} from '../../../declarations/constants/declarations-status';
 
 @Component({
-  selector: 'app-requests-registrations',
-  imports: [],
-  templateUrl: './requests-registrations.component.html',
-  styleUrl: './requests-registrations.component.scss'
+    selector: 'app-requests-registrations',
+    imports: [
+        IconFieldModule,
+        InputIconModule,
+        InputTextModule,
+        TableModule,
+        PopoverModule,
+        ButtonModule,
+        CurrencyPipe,
+        TableSkeletonComponent
+    ],
+    providers: [AlertsService, ConfirmationService, DialogService],
+    templateUrl: './requests-registrations.component.html',
+    styleUrl: './requests-registrations.component.scss'
 })
-export class RequestsRegistrationsComponent {
+export class RequestsRegistrationsComponent implements OnInit {
+    @ViewChild('table') table: any | undefined;
+
+    private alertsService = inject(AlertsService);
+    private dialogService = inject(DialogService);
+    private dialogRef: DynamicDialogRef | undefined;
+    private router = inject(Router);
+
+    public isLoading = false;
+
+    ngOnInit() {
+
+    }
+
+    get
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.table?.filterGlobal(filterValue, 'contains');
+    }
 
 }
