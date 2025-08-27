a8f97b321ca9c894309aaa0b5383f6b9f8245bb3
Juan Amaya <97049133+jamayak@users.noreply.github.com>
2025-08-27 15:59:29 -0500

    feat: aperturas por empresa

--- src/app/features/configurations/pages/opening-by-company/opening-by-company.component.ts
+++ src/app/features/configurations/pages/opening-by-company/opening-by-company.component.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, ViewChild} from '@angular/core';
+import {IconField, IconFieldModule} from 'primeng/iconfield';
+import {InputIcon, InputIconModule} from 'primeng/inputicon';
+import {InputText, InputTextModule} from 'primeng/inputtext';
+import {Button, ButtonModule} from 'primeng/button';
+import {Popover, PopoverModule} from 'primeng/popover';
+import {ConfirmationService, PrimeTemplate} from 'primeng/api';
+import {TableModule} from 'primeng/table';
+import {TableSkeletonComponent} from '../../../../shared/components/skeleton/table-skeleton/table-skeleton.component';
+import {CompaniesStatus} from '../../../companies/constants/companies-status';
+import {CompaniesService} from '../../../companies/services/companies.service';
+import {AlertsService} from '../../../../core/services/alerts.service';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-opening-by-company',
-  imports: [],
-  templateUrl: './opening-by-company.component.html',
-  styleUrl: './opening-by-company.component.scss'
+    selector: 'app-opening-by-company',
+    imports: [
+        IconFieldModule,
+        InputIconModule,
+        InputTextModule,
+        TableModule,
+        PopoverModule,
+        ButtonModule,
+        TableSkeletonComponent
+    ],
+    providers: [AlertsService, ConfirmationService],
+    templateUrl: './opening-by-company.component.html',
+    styleUrl: './opening-by-company.component.scss'
 })
-export class OpeningByCompanyComponent {
+export class OpeningByCompanyComponent implements OnInit{
 
+    @ViewChild('table') table: any | undefined;
+
+    private companiesService = inject(CompaniesService);
+    private alertsService = inject(AlertsService);
+    private router = inject(Router);
+
+    public companies: any;
+    public isLoading: boolean = false;
+
+    public companiesStatus = CompaniesStatus;
+
+    ngOnInit() {
+        this.getDraftDeclarations()
+    }
+
+    public getDraftDeclarations() {
+        this.isLoading = true;
+        this.companiesService.getCompaniesByStatus('all').subscribe({
+            next: res => {
+                this.isLoading = false;
+                this.companies = res.companies;
+            },
+            error: err => {
+                this.isLoading = false;
+                this.alertsService.errorAlert(err.error.errors);
+            }
+        });
+    }
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.table?.filterGlobal(filterValue, 'contains');
+    }
 }
--- src/app/features/reports/pages/reports-year-month/reports-year-month.component.ts
+++ src/app/features/reports/pages/reports-year-month/reports-year-month.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Months} from '../../../../core/constants/months';
+import {SelectModule} from 'primeng/select';
+import {ButtonModule} from 'primeng/button';
+import {ReportsService} from '../../services/reports.service';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {AlertsService} from '../../../../core/services/alerts.service';
+import {ConfirmationService} from 'primeng/api';
 
 @Component({
-  selector: 'app-reports-year-month',
-  imports: [],
-  templateUrl: './reports-year-month.component.html',
-  styleUrl: './reports-year-month.component.scss'
+    selector: 'app-reports-year-month',
+    imports: [
+        SelectModule,
+        ButtonModule,
+        ReactiveFormsModule,
+    ],
+    providers: [AlertsService, ConfirmationService],
+    templateUrl: './reports-year-month.component.html',
+    styleUrl: './reports-year-month.component.scss'
 })
-export class ReportsYearMonthComponent {
+export class ReportsYearMonthComponent implements OnInit {
 
+    public reportForm: FormGroup;
+
+    private reportsService = inject(ReportsService);
+    private spinner = inject(NgxSpinnerService);
+    private alertsService = inject(AlertsService);
+    private formBuilder = inject(FormBuilder);
+
+    public years: any = ['2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025']
+    public months = Months;
+
+    ngOnInit() {
+        this.initReportForm();
+        this.years = this.generateYearsArray();
+    }
+
+    initReportForm() {
+        this.reportForm = this.formBuilder.group({
+            year: ['', Validators.required],
+            month: ['', Validators.required]
+        });
+    }
+
+    generateReport() {
+        this.spinner.show();
+        const data = this.reportForm.value;
+        this.reportsService.generateGeneralReport(data).subscribe({
+            next: data => {
+                this.spinner.hide();
+                const file = URL.createObjectURL(data)
+                window.open(file, '_blank');
+            },
+            error: err => {
+                this.spinner.hide();
+                this.alertsService.errorAlert(err.error.errors);
+            }
+        })
+    }
+
+    public generateYearsArray(): any[] {
+        const currentYear = new Date().getFullYear();
+        const yearsArray = [];
+
+        for (let year = 2017; year <= currentYear; year++) {
+            yearsArray.push({year: year.toString()});
+        }
+
+        return yearsArray;
+    }
 }
