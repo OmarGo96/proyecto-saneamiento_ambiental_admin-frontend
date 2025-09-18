08e386cf621444604fe48214f67505bb6d6abd78
Omar <omar@gusacapital.com>
2025-09-18 09:38:52 -0500

    Se adjunta cambio de url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    urlApi: 'https://saneamientoambiental.playadelcarmen.gob.mx/dsa/api',
+    mapboxToken: 'pk.eyJ1IjoianVhbmFtYXlhayIsImEiOiJjbHZoNG5oZzIxMjlkMmlzMms4b2Q1aHNhIn0.6MnNN5l2rogqVZbgTb03gw',
+};
